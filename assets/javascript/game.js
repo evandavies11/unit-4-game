609ff80afcc6485ab8bb5bc39eb838ddb9990d79
EVAN DAVIES <evandavies@EVANs-MacBook-Pro-2.local>
2019-06-26 23:42:20 -0500

    new outline

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,23 @@
 console.log("hellow world")
+
+var randomResult;
+var losses;
+var wins;
+
+for (var i = 0; i < 4; i++) {
+
+    var crystal = $("<div>");
+
+    $(".crystals").append(crystal);
+
+
+}
+// game that uses crystals to get random result
+// four crystals
+// each crystal generates a random number between 1 and 12
+// computer creates a new random number after player has won or lost
+// when crystal is clicked it adds its number to user score
+// until it equals the total score
+// if it gets over the random result we add to loss counter
+// if it is equal we increment the loss counter
+
